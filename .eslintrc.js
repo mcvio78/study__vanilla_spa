bf7e0ce30ab7ee335cbee55feba75d11f689da39
Mauro <mcvio78@gmail.com>
2020-03-04 10:15:22 +0100

    improve material design icons compatibility, improve style with links and font style, navigation bar style readability and create new colors, create rates-page, loader-page and create their style, add favicon and eslint rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
 	"env": {
 		"browser": true,
+		"commonjs": true,
 		"node": true,
-		"es6": true
+		"es6": true,
+		"jquery": true
 	},
 	"extends": "eslint:recommended",
 	"globals": {
@@ -18,6 +20,6 @@ module.exports = {
 		"quotes": [1, "single", { "avoidEscape": true }],
 		"semi": 1,
 		"no-useless-escape": 0,
-		"no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 0 }]
+		"no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }]
 	}
 };
--- src/js/app.js
+++ src/js/app.js
@@ -4,22 +4,24 @@ import './../sass/main.scss';
 window.addEventListener('load', () => {
 	const el = $('#app');
 
+	/* eslint-disable */
 	// Compile Handlebar Templates
-	const errorTemplate = Handlebars.compile($('#error-template').html());
-	const ratesTemplate = Handlebars.compile($('#rates-template').html());
-	const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
-	const historicalTemplate = Handlebars.compile($('#historical-template').html());
+	const errorTemplate = Handlebars.compile($('#handlebars-error-template').html());
+	const ratesTemplate = Handlebars.compile($('#handlebars-rates-template').html());
+	const exchangeTemplate = Handlebars.compile($('#handlebars-exchange-template').html());
+	const historicalTemplate = Handlebars.compile($('#handlebars-historical-template').html());
+	const loaderTemplate = Handlebars.compile($('#handlebars-loader-template').html());
+	/* eslint-enable */
 
 	// const html = ratesTemplate();
 	// el.html(html);
 
 	// Router Declaration
-	const router = new Router({
+	const router = new Router({ /* eslint-disable-line */
 		mode: 'history',
 		// Default: Callback function for 404 page
 		page404: (path) => {
 			const html = errorTemplate({
-				color: 'yellow',
 				title: 'Error 404 - Page NOT Found!',
 				message: `The path '/${path}' does not exist on this site`,
 			});
@@ -28,9 +30,69 @@ window.addEventListener('load', () => {
 		},
 	});
 
-	router.add('/', () => {
-		let html = ratesTemplate();
+	// Instantiate api handler
+	// API client for communicating with our proxy server [(2)proxy server]
+	const api = axios.create({ /* eslint-disable-line */
+		baseURL: 'http://localhost:3000/api',
+		timeout: 5000,
+	});
+
+	// Display Error Banner - destructuring variables {title, message} mount errorTemplate passing red, title and message.
+	const showError = error => {
+
+		//----------------------------------------------------------------------------------------------ERROR HANDLER STEP 5
+		// error   --->   Error: Request failed with status code 500
+
+		// error.response   --->  data: {title: "An unexpected error occurred", message: "invalid_base_currency"}
+
+		// error.response.data   --->   {title: "An unexpected error occurred", message: "invalid_base_currency"}
+
+		//---------------------------------------------------------------------DESTRUCTURING AND SEND IT INSIDE HANDLEBARS 6
+		const { title, message } = error.response.data;
+		const html = errorTemplate({ title, message });
+
 		el.html(html);
+
+		const errorPage = $('#error-page');
+		errorPage.addClass('server-error');
+	};
+
+	/**
+	 *******************************************************************************************************CURRENCY RATES
+	 */
+	// Display Latest Currency Rates
+	router.add('/', async () => {
+
+		// Display loader first
+		let html = ratesTemplate();
+
+		// McFix try to avoid fouc.
+		let loader = loaderTemplate();
+		el.html(loader);
+
+		try {
+			// Load Currency Ratesconst
+			// Call PROXY SERVER = http://localhost:3000/
+			// Call APIs PROXY SERVER = http://localhost:3000/api ( in this case /rates )
+			const response = await api.get('/rates');
+			// DESTRUCTURING = base: EUR, date: 2020-03-03, rates: EUR: 1, USD: 1.11625..
+			const { base, date, rates } = response.data;
+
+			// Di'/rates'// Display Rates Table
+			html = ratesTemplate({ base, date, rates });
+			el.html(html);
+
+		} catch (error) {
+			//--------------------------------------------------------------------------------------------ERROR HANDLER STEP 4
+			// Call show error function error handler.
+			// NODE default error handler (err, req, res, next)
+			// error = Error: Request failed with status code 500 because ( res.status(500).send({ title:... )
+			showError(error);
+		} finally {
+
+			// Remove loader status
+			//$('.loading').removeClass('loading');
+		}
 	});
 
 	router.add('/exchange', () => {
